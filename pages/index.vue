2f4e1d517063ef8f0a60b24453da36cb16b4b096
Denisovart <topyktdoto@mail.ru>
2024-04-09 20:47:46 +0900

    patch(): 0.0.19

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-[calc(100vh-140px)] w-screen bg-emerald-500 flex flex-col gap-20 items-center justify-center">
-    <span class="text-3xl text-white">FOOD RANKING!</span>
-    <NuxtLink to="/about">Про sdsdsнас</NuxtLink>
+    <span class="text-3xl text-white">FOOD RANKING 0.0.19!</span>
+    <NuxtLink to="/about">Про нас</NuxtLink>
   </div>
 </template>
