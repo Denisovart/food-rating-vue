c1d47babebd497d19406d4ec027d8c74f5e9aad8
Denisovart <topyktdoto@mail.ru>
2024-04-09 20:15:32 +0900

    test webhook

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,6 @@ import { NuxtLink } from '#build/components';
 <template>
   <div class="h-screen w-screen bg-emerald-500 flex flex-col gap-20 items-center justify-center">
     <span class="text-3xl text-white">FOOD RATING YKT!</span>
-    <NuxtLink to="/about">Про команду</NuxtLink>
+    <NuxtLink to="/about">Про командуasdasd</NuxtLink>
   </div>
 </template>
