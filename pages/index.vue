19494a23d218a7d9c8dbe107e62c1d60e78f2b82
Denisovart <topyktdoto@mail.ru>
2024-04-09 20:26:39 +0900

    feat(main): text edit

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 import { NuxtLink } from '#build/components';
 <template>
   <div class="h-screen w-screen bg-emerald-500 flex flex-col gap-20 items-center justify-center">
-    <span class="text-3xl text-white">FOOD RATING YKT!</span>
+    <span class="text-3xl text-white">CULTURE SEA!</span>
     <NuxtLink to="/about">Про нас</NuxtLink>
   </div>
 </template>
