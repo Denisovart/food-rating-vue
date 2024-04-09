ae4ace45036161b92e5080aa9f78c90fdf83b8c2
Denisovart <topyktdoto@mail.ru>
2024-04-09 20:37:56 +0900

    asdasd

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen w-screen bg-emerald-500 flex flex-col gap-20 items-center justify-center">
     <span class="text-3xl text-white">FOOD RANKING!</span>
-    <NuxtLink to="/about">Про нас</NuxtLink>
+    <NuxtLink to="/about">Про sdsdsнас</NuxtLink>
   </div>
 </template>
