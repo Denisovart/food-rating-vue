17adcce230b94e50b85a4de3e1faed5fbd5485a9
Denisovart <topyktdoto@mail.ru>
2024-04-09 20:35:35 +0900

    feat(index): delete

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,3 @@
-
-import { NuxtLink } from '#build/components';
 <template>
   <div class="h-screen w-screen bg-emerald-500 flex flex-col gap-20 items-center justify-center">
     <span class="text-3xl text-white">FOOD RANKING!</span>
